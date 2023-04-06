b2a6f139e32d8e77b72ced0dd278bd5051a7d0d5
Никита Александрович <49814202+ragevna@users.noreply.github.com>
2023-04-07 00:25:02 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -11,13 +11,18 @@ openssl req -x509 -newkey rsa:4096 -keyout ca.key -out ca.crt -subj "/C=$C/ST=$S
 openssl req -newkey rsa:4096 -passout pass:"$pass" -config openssl.cnf -keyout server.key -out server.csr -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=iwtm/emailAddress=$EMAIL" 2>/dev/null
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -extfile openssl.cnf -extensions server_cert -days 365 -passin pass:"$pass" -out server.crt 2>/dev/null
 openssl pkcs12 -export -out bundle.pfx -inkey ca.key -in server.crt -in ca.crt -passout pass:"$pass" -passin pass:"$pass" 2>/dev/null
-echo "xxXX1234" >> pass.pass
+echo "xxXX1234" >> /opt/iw/tm5/etc/certification/pass.pass
 
 cp /etc/nginx/conf.d/iwtm.conf /etc/nginx/conf.d/iwtm.conf.bak
-sed -i 's#ssl_certificate /opt/iw/tm5/etc/certification/web-server.pem;#ssl_certificate /ca/server.crt;#' /etc/nginx/conf.d/iwtm.conf
-sed -i 's#ssl_certificate_key /opt/iw/tm5/etc/certification/web-server.key;#ssl_certificate_key /ca/server.key;#' /etc/nginx/conf.d/iwtm.conf
-sed -i '/ssl_certificate_key/a ssl_password_file /ca/pass.pass;' /etc/nginx/conf.d/iwtm.conf
 
-rm -rf ca.crt ca.srl ca.key server.csr
+openssl x509 -in mycert.crt -out mycert.pem -outform PEM
+mv /ca/server.crt /opt/iw/tm5/etc/certification/web-server.pem
+mv /ca/server.crt /opt/iw/tm5/etc/certification/web-server.key
+sed -i '/ssl_certificate_key/a ssl_password_file /opt/iw/tm5/etc/certification/pass.pass;' /etc/nginx/conf.d/iwtm.conf
+
+# sed -i 's#ssl_certificate /opt/iw/tm5/etc/certification/web-server.pem;#ssl_certificate /ca/server.crt;#' /etc/nginx/conf.d/iwtm.conf
+# sed -i 's#ssl_certificate_key /opt/iw/tm5/etc/certification/web-server.key;#ssl_certificate_key /ca/server.key;#' /etc/nginx/conf.d/iwtm.conf
+
+rm -rf ca.* server.*
 systemctl restart nginx
 echo -e "THE SCRIPT HAS FINISHED ITS WORK. \n\nCOPY bundle.pfx TO 'demo.lab'"
