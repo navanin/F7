c29b718e06b2cdc27b4d24a44775ef6e05d7a114
Никита Александрович <49814202+ragevna@users.noreply.github.com>
2023-04-07 00:32:42 +0700

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -7,7 +7,7 @@ OU=IT
 EMAIL=support@demo.lab
 pass=xxXX1234
 
-curl https://raw.githubusercontent.com/ragevna/F7/main/openssl.cnf >> /ca/openssl.cnf
+curl -s https://raw.githubusercontent.com/ragevna/F7/main/openssl.cnf >> /ca/openssl.cnf
 
 openssl req -x509 -newkey rsa:4096 -keyout ca.key -out ca.crt -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=Demo.lab RootCA/emailAddress=$EMAIL" -days 3650 -extensions v3_ca  -passout pass:"$pass" 2>/dev/null
 openssl req -newkey rsa:4096 -passout pass:"$pass" -config openssl.cnf -keyout server.key -out server.csr -subj "/C=$C/ST=$ST/L=$L/O=$O/OU=$OU/CN=iwtm/emailAddress=$EMAIL" 2>/dev/null
